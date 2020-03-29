8b4c885d2ce421f56594b3e602ea4bd664339f51
Jan Konarzewski <jan.konarzewski@voiapp.io>
2020-03-29 13:57:22 +0200

    edit .yaml files

--- internal/worker.go
+++ internal/worker.go
@@ -3,19 +3,20 @@ package internal
 import (
 	"bytes"
 	"fmt"
-	"github.com/gammazero/workerpool"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"sync"
+
+	"github.com/gammazero/workerpool"
 )
 
 const (
 	// TODO: fetch from go release
 	latest = "1.14"
-	goMod = "go.mod"
+	goMod  = "go.mod"
 )
 
 type file struct {
@@ -24,14 +25,14 @@ type file struct {
 }
 
 type Worker struct {
-	path       string
+	path      string
 	currentGo string
-	files      []file
+	files     []file
 }
 
 func NewWorker(url string) Worker {
 	return Worker{
-		path:       url,
+		path: url,
 	}
 }
 
@@ -41,7 +42,7 @@ func (w Worker) Init() {
 
 	for i := 0; i < 1; i++ {
 		wg.Add(1)
-		wp.Submit(func (){
+		wp.Submit(func() {
 			w.bump(&wg)
 		})
 	}
@@ -55,11 +56,22 @@ func (w Worker) bump(wg *sync.WaitGroup) {
 		haltOnError(err)
 	}
 
+	// no go.mod with version exit
+	if w.currentGo == "" {
+		return
+	}
+
 	if err := w.vendor(); err != nil {
 		haltOnError(err)
 	}
 
-	if err := filepath.Walk(w.path, w.visitGitHub); err != nil {
+	if err := w.visitGitHub(); err != nil {
+		haltOnError(err)
+	}
+
+	// TODO: check if hub installed
+	if err := w.submit(); err != nil {
+		fmt.Println("submit")
 		haltOnError(err)
 	}
 
@@ -87,7 +99,6 @@ func (w *Worker) visit(path string, fi os.FileInfo, err error) error {
 
 	matched, err := filepath.Match("go.mod", fi.Name())
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 
@@ -100,34 +111,17 @@ func (w *Worker) visit(path string, fi os.FileInfo, err error) error {
 	return nil
 }
 
-// TODO: optimize so only visits the .github folder
-func (w *Worker) visitGitHub(path string, fi os.FileInfo, err error) error {
-	if err != nil {
-		return err
-	}
-
-	if fi.IsDir() && fi.Name() != "vendor" {
-		return filepath.SkipDir
-	}
-
-	if fi.IsDir() && fi.Name() == ".git" {
-		return filepath.SkipDir
-	}
-
-	if fi.IsDir() {
-		return nil
-	}
-
-	fmt.Println(fi.Name())
-	matched, err := filepath.Match("*", fi.Name())
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	if matched {
-		if err := w.editFile(path); err != nil {
+func (w *Worker) visitGitHub() error {
+	for _, file := range w.files {
+		matched, err := filepath.Match("*.yaml", file.name)
+		if err != nil {
 			return err
+		}
+
+		if matched {
+			if err := w.editFile(file.path); err != nil {
+				return err
+			}
 		}
 	}
 
@@ -175,9 +169,80 @@ func (w *Worker) vendor() error {
 	return nil
 }
 
+func (w *Worker) submit() error {
+	if err := w.addChanges(); err != nil {
+		fmt.Println("addChanges")
+		return err
+	}
+
+	if err := w.branchOut(); err != nil {
+		fmt.Println("branchOut")
+		return err
+	}
+
+	if err := w.commit(); err != nil {
+		fmt.Println("commit")
+		return err
+	}
+
+	if err := w.pr(); err != nil {
+		fmt.Println("pr")
+		return err
+	}
+
+	return nil
+}
+
+func (w *Worker) branchOut() error {
+	cmd := exec.Command("hub", "checkout", "-b", "nextGo")
+	cmd.Dir = filepath.Join(w.path)
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (w *Worker) commit() error {
+	cmd := exec.Command("hub", "commit", `-am "bump go version with go-bump"`)
+	cmd.Dir = filepath.Join(w.path)
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (w *Worker) pr() error {
+	cmd := exec.Command("hub",
+		"pull-request",
+		"-p",
+		"-l minor",
+		"--no-edit",
+	)
+	cmd.Dir = filepath.Join(w.path)
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (w *Worker) addChanges() error {
+	cmd := exec.Command("hub", "add", ".")
+	cmd.Dir = filepath.Join(w.path)
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func haltOnError(err error) {
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		if _, err := fmt.Fprintln(os.Stderr, err); err != nil {
+			fmt.Println(err)
+		}
 		os.Exit(1)
 	}
 }
